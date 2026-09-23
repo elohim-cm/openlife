08c121162cefa9a1ea44a7751c958a271318fa15
elohim-cm <elohimwarren.cm@gmail.com>
2026-09-23 16:39:28 +0100

    configure API base

--- utils/api/api.js
+++ utils/api/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const BASE_URL = process.env.BASE_URL;
 export const WEB_URL = process.env.NEXT_PUBLIC_WEB_URL ?? 'https://openlife.acamvie.com/';
 //export const BASE_URL = "http://127.0.0.1:8000/api/v1";
 //export const BASE_URL = "http://92.243.21.82:8050/api/v1";
